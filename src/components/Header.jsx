1ffcac95015281e077b8decf6f62665ea064305e
chocoscoding <chocoscoding@gmail.com>
2022-02-08 23:08:33 +0100

    add section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-  <div>
-      Header
-  </div>
+  <Container>
+      
+      <a href="#">
+        <img src="/images/logo.svg" alt="logo" />
+      </a>
+      <Menu>
+
+      </Menu>
+  </Container>
   )
 };
 
 export default Header;
+
+const Container = styled.div`
+
+`
+const Menu = styled.div`
+
+`
